cd9f9485ce6a54ff78c3ba493426da533c764fd7
Annisa Rizki Pratiwi Wibowo <annisaarizki@yahoo.co.id>
2015-09-17 00:38:07 +0800

    Update list.cpp

--- LinkedList/list.cpp
+++ LinkedList/list.cpp
@@ -27,6 +27,23 @@ void buat_baru(infotype *x)
     *x=k;
 }
 
+void insertfirst (list *l, address p)
+{
+    if((*l).awal==NULL)
+    {
+        (*l).awal=p;
+        next((*l).awal)=NULL;
+
+
+    }
+    else
+    {
+        next(p)=NULL;
+        next(p)=(*l).awal;
+        (*l).awal=p;
+    }
+}
+
 void insertafter(list *l, address p, address s)
 {
     address Q;
@@ -39,6 +56,14 @@ void insertafter(list *l, address p, address s)
     next(p)=next(Q);
     next(Q)=p;
 
+}
+
+void deletefirst(list *l,address *p)
+{
+    *p=(*l).awal;
+    (*l).awal=next((*l).awal);
+    next(*p)=NULL;
+    delete p;
 }
 
 void cariid(infotype *x)
